7f14d416a500e4fdfebd55876f4c259191404572
butoijooo <125478220+butoijooo@users.noreply.github.com>
2024-03-03 04:42:15 -0800

    Update nomachine-xfce4.sh

--- nomachine-xfce4.sh
+++ nomachine-xfce4.sh
@@ -32,7 +32,7 @@ read -p "choose ngrok region: " CRP
 ./ngrok tcp --region $CRP 4000 &>/dev/null &
 sleep 1
 if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
-docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD=11111 -e USER=bacox --cap-add=SYS_PTRACE --shm-size=1g christian773/xfce-vnc:xfce4
+docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD=11111 -e USER=bacox --cap-add=SYS_PTRACE --shm-size=1g thuonghai2711/nomachine-ubuntu-desktop:xfce4
 echo "NoMachine: https://www.nomachine.com/download"
 echo Done! NoMachine Information:
 echo IP Address:
