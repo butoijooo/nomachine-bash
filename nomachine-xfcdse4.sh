dd557441af9a65406dcced25f582f5c5a8346407
butoijooo <125478220+butoijooo@users.noreply.github.com>
2024-03-03 04:24:29 -0800

    Add files via upload

---
+++ nomachine-xfcdse4.sh
@@ -0,0 +1,47 @@
+wget -O ng.sh https://raw.githubusercontent.com/butoijooo/nomachine-bash/main/ngrok.sh > /dev/null 2>&1
+chmod +x ng.sh
+./ng.sh
+
+
+function goto
+{
+    label=$1
+    cd 
+    cmd=$(sed -n "/^:[[:blank:]][[:blank:]]*${label}/{:a;n;p;ba};" $0 | 
+          grep -v ':$')
+    eval "$cmd"
+    exit
+}
+
+: ngrok
+clear
+echo "Go to: https://dashboard.ngrok.com/get-started/your-authtoken"
+read -p "Paste Ngrok Authtoken: " CRP
+./ngrok authtoken $CRP 
+
+clear
+echo "SILESSUREKKU MALEBBIE (GUNAKAN SEPERLUNYA)"
+echo "======================="
+echo "MASUKKAN TOKENMU YAGH (LIMIT 1GB /Month)."
+echo "======================="
+echo "Pilih ap ya"
+echo "jangan salah"
+echo "Nanti lambat"
+echo "maksimal 6 jam"
+echo "backup data sebelum 6 jam)"
+echo "Selamat menikmati RDP Full acces"
+echo "OK kaka"
+read -p "choose ngrok region: " CRP
+./ngrok tcp --region $CRP 4000 &>/dev/null &
+sleep 1
+if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
+docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD=11111 -e USER=bacox --cap-add=SYS_PTRACE --shm-size=1g thuonghai2711/nomachine-ubuntu-desktop:xfce4
+clear
+echo "NoMachine: https://www.nomachine.com/download"
+echo Done! NoMachine Information:
+echo IP Address:
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p' 
+echo User: bacox
+echo Passwd: 11111
+echo "Jika tidak jalan mulai dari awal kemungkinan salah kode"
+seq 1 43200 | while read i; do echo -en "\r Running .     $i s /43200 s";sleep 0.1;echo -en "\r Running ..    $i s /43200 s";sleep 0.1;echo -en "\r Running ...   $i s /43200 s";sleep 0.1;echo -en "\r Running ....  $i s /43200 s";sleep 0.1;echo -en "\r Running ..... $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1;echo -en "\r Running  .... $i s /43200 s";sleep 0.1;echo -en "\r Running   ... $i s /43200 s";sleep 0.1;echo -en "\r Running    .. $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1; done
